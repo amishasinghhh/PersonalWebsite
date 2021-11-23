c0288b8efe466e2760352a8dacab0752d2187098
Singh Amisha <amishasingh9976@gmail.com>
2021-11-23 15:56:51 -0500

    Create header

---
+++ src/components/aboutme.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './aboutme.css'
+
+class AboutMe extends React.Component{
+    render(){
+        return (
+            <>
+            <div>
+            </div>
+            </>
+            
+        );
+    }
+}
+
+export default AboutMe;
---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './header.css'
+
+class Header extends React.Component{
+    render(){
+        return (
+            <>
+            <div>
+            <h1 class = "Hi">Hi! I'm</h1>
+            <div class = "first Amisha">Amisha Singh</div>
+            </div>
+            </>
+        );
+    }
+}
+
+
+export default Header;
